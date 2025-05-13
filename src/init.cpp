dcd8fe35c99bd8f31da7d5ca5e8c462c8a448656
Arthur-91140 <96424611+Arthur-91140@users.noreply.github.com>
2025-05-13 10:08:03 +0200

    add GetUserFolderPath function to retrieve user folder path

--- src/init.cpp
+++ src/init.cpp
@@ -20,3 +20,12 @@ void banner() {
     std::cout << "        ░  ░░ ░                ░           ░  ░     ░            ░      " << std::endl;
     std::cout << "            ░                                                           " << std::endl;
 }
+
+// Fonction pour obtenir le dossier utilisateur
+string GetUserFolderPath() {
+    char path[MAX_PATH];
+    if (SHGetFolderPath(NULL, CSIDL_PROFILE, NULL, 0, path) == S_OK) {
+        return string(path);
+    }
+    return "";
+}
